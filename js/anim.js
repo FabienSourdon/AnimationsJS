a6bd6351fb70db7884b6717ed47e0de0a0ce92ef
Fabien Sourdon <fabien.sourdon@gmail.com>
2020-06-23 10:11:53 +0200

    Modif23062020

--- js/anim.js
+++ js/anim.js
@@ -1,17 +1,30 @@
+let points = document.getElementById("points");
+let plusTxt = document.getElementById("plus");
+let btnPlus = document.getElementById("btnPlus");
+let objExpand = document.getElementById("divExpand");
+
+console.log(btnPlus);
+
 function expand() {
-    var points = document.getElementById("points");
-    var plusTxt = document.getElementById("plus");
-    var btnPlus = document.getElementById("btnPlus");
-    var objExpand = document.getElementById("blocTxt");
 
     if (points.style.display === "none") {
-        points.style.display = "flex";
+
+        points.style.display = "inline";
         btnPlus.innerHTML = "Voir plus...";
-        plusTxt.style.display = "none";
+        plusTxt.style.visibility = "hidden";
+        gsap.to("#divExpand", { duration: 0.2, height: objExpand.offsetHeight - 324 })
+        btnPlus.remove();
+        points.append(btnPlus);
     }
     else {
+
         points.style.display = "none";
         btnPlus.innerHTML = "Voir moins...";
-        plusTxt.style.display = "inline";
+        plusTxt.style.visibility = "visible";
+        gsap.from("#plus", { duration: 1, opacity: 0 });
+        gsap.to("#divExpand", { height: objExpand.offsetHeight + 324 })
+        gsap.to("#btnPlus", { y: btnPlus.top + 324 })
+        btnPlus.remove();
+        plusTxt.append(btnPlus);
     }
 }
